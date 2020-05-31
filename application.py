523a572ea7055dada024c503932130814f9f67d7
adrian <sc19acyl@leeds.ac.uk>
2020-05-31 22:31:04 +0800

    create chatroom function added

--- application.py
+++ application.py
@@ -1,26 +1,44 @@
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY") or "secret_key"
 socketio = SocketIO(app)
-
+rooms = {}
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        chatroom = request.form.get("room-name")
+        print(chatroom)
+        rooms[chatroom] = {'messages': []}
+    print(rooms)
     return render_template("index.html")
+
+
+@app.route("/chat/<string:room>")
+def chat_room(room):
+    try:
+        messageList = rooms[room]['messages']
+    except KeyError:
+        flash("No room found, please create a new room")
+        return redirect(url_for('index'))
+    print(messageList)
+    return render_template("chat.html", messages=messageList)
 
 
 @app.route("/chat")
 def chat():
     return render_template("chat.html")
 
+
 @socketio.on("send message")
 def send(data):
     username = data['username']
     message = data['message']
     time = data['time']
-    print("sending data")
+    roomname = data['roomname']
+    rooms[roomname]['messages'].append({'username': username, 'message': message, 'time': time})
     emit('announce message', {'username': username, 'message': message, 'time': time}, broadcast=True)
--- static/chat.js
+++ static/chat.js
@@ -1,14 +1,31 @@
 const username = localStorage.getItem('username');
+var pathname = window.location.pathname.split('/');
+const roomname = pathname[2];
+
 
 document.addEventListener('DOMContentLoaded', () => {
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+    document.querySelector('#submit').disabled = true;
+
+    document.querySelector('#message').onkeyup = () => {
+        if (document.querySelector('#message').value.length > 0) {
+            document.querySelector('#submit').disabled = false;
+        }
+        else {
+            document.querySelector('#submit').disabled = true;
+        }
+    }
 
     socket.on('connect', () => {
 
         document.querySelector('#chatbox').onsubmit = () => {
             const message = document.querySelector('#message').value;
             
-            var jsonMessage = {'username': username, 'message': message, 'time': Date()};
+            document.querySelector('#message').value = '';
+            document.querySelector('#submit').disabled = true;
+
+            var jsonMessage = {'username': username, 'message': message, 'time': Date(), 'roomname': roomname};
             
             socket.emit('send message', jsonMessage);
             return false;
